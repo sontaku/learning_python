13f8cf2a62c78729389052e0c11dd95e319c66d3
jyson19 <jyson19@gmail.com>
2021-05-10 14:20:01 +0900

    2021-05-10

--- 01_basic/a_datatype_class/Ex07_set.py
+++ 01_basic/a_datatype_class/Ex07_set.py
@@ -8,4 +8,15 @@
 s = set()               # 빈 집합을 생성
 s = set([1,2,3,1,1])
 print(s)
+# print(s[0])
 
+s3 = {3,4,5,6}
+print(s.intersection(s3))
+print(s.union(s3))
+
+print('-----------------')
+
+print(s & s3) # 교집합
+print(s | s3) # 합집합
+print(s - s3) # 차집합
+print(s3 - s) # 여집합
